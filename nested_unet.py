f79d5829dd9f7e9573b2936ba5d8a4ecc3aef2c6
Sravani Kilari <kilarisravani77@gmail.com>
2024-10-01 09:46:44 +0530

    Create nested_unet.py

---
+++ nested_unet.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NestedUNet(nn.Module):
+    def __init__(self, num_classes=1, input_channels=1):
+        super(NestedUNet, self).__init__()
+        
+    def forward(self, x):
+        pass
